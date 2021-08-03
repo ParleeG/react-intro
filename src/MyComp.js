d08b00f06393a29d7ae6b787fff28f3b0c02aaa0
Himanshu Gupta <guptahimanshu0520@gmail.com>
2021-08-03 21:06:46 +0530

    adding mycomp

--- src/MyComp.js
+++ src/MyComp.js
@@ -4,12 +4,18 @@ import React from "react";
 
 class MyComp extends React.Component {
   state = {
-    someNumber: 3,
+    someNumber: 0,
   };
 
   render = () => {
     return (
       <div>
+        <button style={{color:"green"}} onClick={()=>{
+            this.setState({someNumber: this.state.someNumber + 1});
+        }}>Increment</button>
+        <button style={{color:"red"}} onClick={()=>{
+            this.setState({someNumber: this.state.someNumber - 1});
+        }}>Decrement</button>
         <h1> {this.state.someNumber} </h1>
       </div>
     );
